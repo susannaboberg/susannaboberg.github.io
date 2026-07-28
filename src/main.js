f387d0733740f9eb1b9f9034c2a5e636d24c90fe
Susanna Boberg <sboberg617@gmail.com>
2026-07-27 20:42:59 -0700

    working scene view

--- src/main.js
+++ src/main.js
@@ -1,60 +1,121 @@
-import './style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.js'
+import './style.scss'
+import * as THREE from 'three';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-document.querySelector('#app').innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+const canvas = document.querySelector("#canvas-experience");
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-<div class="ticks"></div>
+// Loaders
+const textureLoader = new THREE.TextureLoader();
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-          <img class="button-icon" src="${javascriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+// Model loader
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('/draco/');
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+const loader = new GLTFLoader();
+loader.setDRACOLoader(dracoLoader);
 
-setupCounter(document.querySelector('#counter'))
+const textureMap = {
+  Base: {
+    day: "/textures/texture_bases_day.webp",
+    night: ""
+  },
+  Target: {
+    day: "/textures/texture_targets_day.webp",
+    night: ""
+  }
+};
+
+//after done loading, put in object
+const loadedTextures = {
+  day:{
+  },
+  night: {},
+}
+
+Object.entries(textureMap).forEach(([key, paths]) => {
+  const dayTexture = textureLoader.load(paths.day);
+  dayTexture.flipY = false;
+  dayTexture.colorSpace = THREE.SRGBColorSpace;
+  loadedTextures.day[key] = dayTexture;
+
+  const nightTexture = textureLoader.load(paths.night)
+  nightTexture.flipY = false;
+  nightTexture.colorSpace = THREE.SRGBColorSpace;
+  loadedTextures.night[key] = nightTexture;
+  nightTexture.flipY = false;
+});
+
+
+loader.load("/models/final-room.glb", (glb)=>{
+  glb.scene.traverse((child) => {
+    if (!child.isMesh) return;
+    const key = child.name === "base" ? "Base" : "Target";
+    child.material = new THREE.MeshBasicMaterial({ map: loadedTextures.day[key]});
+  
+    scene.add(glb.scene); 
+  });
+});
+
+const scene = new THREE.Scene();
+const aspect = window.innerWidth / window.innerHeight;
+const frustumSize = 20;
+
+const camera = new THREE.OrthographicCamera(
+    frustumSize * aspect / -2, // left
+    frustumSize * aspect / 2,  // right
+    frustumSize / 2,           // top
+    frustumSize / -2,          // bottom
+    0.1,                       // near
+    1000                       // far
+);
+
+camera.position.set(10, 5.5, 10);
+camera.lookAt(0, 0, 0);
+
+const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+//Event Listeners
+window.addEventListener("resize", ()=>{
+  const aspect = window.innerWidth / window.innerHeight;
+    
+    camera.left = frustumSize * aspect / -2;
+    camera.right = frustumSize * aspect / 2;
+    camera.top = frustumSize / 2;
+    camera.bottom = frustumSize / -2;
+    
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+})
+
+const axesHelper = new THREE.AxesHelper( 5 );
+axesHelper.setColors(
+    0xff0000, // X-axis hex (Red)
+    0x00ff00, // Y-axis hex (Green)
+    0x0000ff  // Z-axis hex (Blue)
+);
+
+scene.add(axesHelper);
+
+
+const render = () => {
+
+  renderer.render( scene, camera );
+
+  window.requestAnimationFrame(render);
+}
+
+render();
